3d310278604b6648161cf63566f3d972823ed10f
Emiiliinjs <168409871+Emiiliinjs@users.noreply.github.com>
2025-10-08 09:39:40 +0300

    Update primary color palette to teal

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                indigo: {
+                    50: '#E5F4F3',
+                    100: '#CCE9E7',
+                    200: '#99D3CE',
+                    300: '#66BCB6',
+                    400: '#33A69D',
+                    500: '#2F9188',
+                    600: '#2B7A78',
+                    700: '#205C59',
+                    800: '#153D3B',
+                    900: '#0A1F1D',
+                    950: '#050F0F',
+                    DEFAULT: '#2B7A78',
+                },
+            },
         },
     },
 
