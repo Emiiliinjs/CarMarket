0a4102984c3e34b6952fde012f26410fa13ab092
Emils Darzins <168409871+Emiiliinjs@users.noreply.github.com>
2026-04-27 09:01:52 +0300

    Add admin seeder for admin@admin.lv

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CarDatabaseSeeder::class);
+        $this->call([
+            CarDatabaseSeeder::class,
+            AdminUserSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
